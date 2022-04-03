851304fcdf163f4ea06e8d63e11105c19016dc39
KELLY LUU <kellyluuu@gmail.com>
2022-04-02 21:51:43 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000,()=>{
+    console.log('pass-it-around open on port 3000')
+})
+
+
+
+app.get("/",(req,res)=>{
+    console.log(randomAmt)
+    res.send(`<html>
+    <body>
+        <h1>99 little bugs in the code</h1>
+        <a href="/98">Take on down, Patch it around</a>
+    </body>
+</html>`)
+})
+
+
+
+app.get("/:number_of_bottles",(req,res)=>{
+    const randomAmt = Math.floor(Math.random() * 10)
+    const randomChance = Math.random()*100;
+    const currentQty = parseInt(req.params.number_of_bottles)
+    const nextQty = currentQty-1
+    const randomAdd = currentQty+randomAmt
+    if (currentQty >0 && randomChance<80) {
+        res.send(`<html>
+        <body>
+            <h1>${currentQty} little bugs in the code</h1>
+            <a href="/${nextQty}">Take on down, Patch it around</a>
+        </body>
+    </html>`)  
+    }else if(currentQty >0 && randomChance>80){
+        res.send(`<html>
+        <body>
+            <h1>${currentQty} little bugs in the code</h1>
+            <a href="/${randomAdd}">Oh no! Found More bugs!</a>
+        </body>
+    </html>`)  
+    }else{
+        res.send(`<html>
+        <body>
+            <h1>0 little bugs in the code</h1>
+            <a href="/">Start Over</a>
+        </body>
+    </html>`)  
+}})
+
+// var d = Math.random();
+// if (d < 0.5)
+//     // 50% chance of being here
+// else if (d < 0.7)
+//     // 20% chance of being here
+// else
+//     // 30% chance of being here
